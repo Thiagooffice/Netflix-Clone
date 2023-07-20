ed41ff7e6b57a11e32e184104c8607850fda899b
Thiago <thiagooffice2@gmail.com>
2023-07-20 12:35:55 -0300

    feat: creating API and nextAuth

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,8 +1,11 @@
 import { useCallback, useState } from "react";
 import { Input } from "../components/Input";
-
+import axios from "axios";
+import {signIn} from "next-auth/react"
+import { useRouter } from "next/router";
 
 export default function Auth() {
+    const router = useRouter()
     const [email, setEmail] = useState("")
     const [name, setName] = useState("")
     const [password, setPassword] = useState("")
@@ -12,6 +15,36 @@ export default function Auth() {
     const toggleVariant = useCallback(() => {
         setVariant((currentVariant) => currentVariant === "login" ? "register" : "login")
     }, [])
+
+    const login  = useCallback(async () => {
+        try {
+          await signIn("credentials", {
+              email,
+              password,
+              redirect: false,
+              callbackUrl: "/"
+          })
+          router.push("/")
+        } catch (error) {
+          console.log(error);
+          
+        }  
+      }, [email, password, router])
+
+    const register = useCallback(async () => {
+
+        try {
+            await axios.post("/api/register", {
+                email,
+                name,
+                password
+            })
+        } catch (error) {
+            console.log(error)
+        }
+        login();
+    }, [email, name, password, login])
+
     return (
         <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
             <div className="bg-black w-full h-full lg:bg-opacity-50">
@@ -46,7 +79,7 @@ export default function Auth() {
                                 value={password}
                             />
                         </div>
-                        <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+                        <button onClick={variant === "login" ? login : register} className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
                             {variant === "login" ? "Login" : "Sign up"}
                         </button>
                         <p className="text-neutral-500 mt-12">
